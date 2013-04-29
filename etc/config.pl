0bf0da43cade6efac888551ad6b802003a032a9b
Yoshimitsu Torii <yoshimitsu.torii@gmail.com>
2013-04-30 07:59:54 +0900

    add info to config.pl

--- etc/config.pl
+++ etc/config.pl
@@ -1,12 +1,25 @@
 {
+    Memcached => +{ servers => [ '127.0.0.1:11211' ] },
     'DB::Master' => [
         'dbi:mysql:dbname=yapc2013',
         'root',
         undef,
-        {
-            RaiseError => 1,
-            AutoCommit => 1,
+        +{
+            AutoCommit          => 1,
+            PrintError          => 0,
+            RaiseError          => 1,
+            ShowErrorStatement  => 1,
+            AutoInactiveDestroy => 1,
+            Callbacks => +{
+                connected => sub {
+                    shift->do('SET SESSION sql_mode=STRICT_TRANS_TABLES');
+                    return;
+                },
+            },
             mysql_enable_utf8 => 1,
         }
     ],
+    FormValidator => {
+        file => app->home->rel_file( "etc/profiles.pl" )
+    }
 }
