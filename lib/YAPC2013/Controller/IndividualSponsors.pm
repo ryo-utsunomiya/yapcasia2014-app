491065315107ba1406bdd2fb8dda493dadfab9cf
Daisuke Maki <lestrrat+github@gmail.com>
2013-06-01 16:42:10 +0900

    It's silly to parse the json file for every request

--- lib/YAPC2013/Controller/IndividualSponsors.pm
+++ lib/YAPC2013/Controller/IndividualSponsors.pm
@@ -2,14 +2,26 @@ package YAPC2013::Controller::IndividualSponsors;
 use Mojo::Base 'YAPC2013::Controller::CRUD';
 use YAPC2013::Constants;
 
+has file => (
+    is => 'rw',
+    default => "var/individual_sponsors.json",
+);
+
 sub index {
     my $self = shift;
-    my $json = $self->get('JSON');
-    open my $fp, "<", "var/individual_sponsors.json";
-    my $data = $json->decode(<$fp>);
-    close $fp;
 
-    $self->render( sponsors => $data );
+    my $cache = $self->get('Memcached');
+    my $cache_key = "faces.json";
+    my $data = $cache->get($cache_key);
+    if (! $data) {
+        my $json = $self->get('JSON');
+        open my $fp, "<", $self->file;
+        $data = $json->decode(do { local $/; <$fp> });
+        close $fp;
+
+        $cache->set($cache_key, $data, 5 * 60);
+    }
+    $self->stash( sponsors => $data );
 }
 
 1;
