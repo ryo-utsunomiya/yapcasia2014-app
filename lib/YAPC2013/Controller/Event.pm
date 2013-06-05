17fd132329f005d9ef07142fb78d0e78c7a8c22c
Daisuke Maki <lestrrat+github@gmail.com>
2013-06-05 13:49:37 +0900

    format start_on

--- lib/YAPC2013/Controller/Event.pm
+++ lib/YAPC2013/Controller/Event.pm
@@ -10,4 +10,25 @@ sub input {
     $self->SUPER::input(@_);
 }
 
+sub commit {
+    my $self = shift;
+    my $member = $self->assert_email or return;
+    my $data = $self->load_from_subsession();
+    if (! $data) {
+        $self->subsession_not_found();
+        return;
+    }
+
+    if (! $data->{is_edit}) {
+        $data->{member_id} = $member->{id};
+    }
+    local $data->{start_on_date};
+    local $data->{start_on_time};
+    $data->{start_on} = sprintf "%s %s",
+        delete $data->{start_on_date},
+        delete $data->{start_on_time}
+    ;
+    $self->SUPER::commit();
+}
+
 1;
