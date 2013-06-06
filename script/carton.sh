d2f8983e705c5626c4c74b0f4ec5d1fcc943ce5b
Daisuke Maki <lestrrat+github@gmail.com>
2013-06-06 10:38:48 +0900

    update default perl to use

--- script/carton.sh
+++ script/carton.sh
@@ -3,7 +3,7 @@
 export MOJO_MODE=production
 export PLACK_ENV=production
 export LC_ALL=en_US
-export PATH=/var/lib/jpa/perl5/perls/perl-5.14.2/bin:/var/lib/jpa/perl5/bin:$PATH
+export PATH=/opt/local/perl-5.16/bin:$PATH
 APP_HOME=/var/lib/jpa/yapcasia.org/yapcasia2013-app
 
 cd $APP_HOME
