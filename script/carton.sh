e60996ab200678034c8c0bff0ce21bcfec207681
Yoshimitsu Torii <yoshimitsu.torii@gmail.com>
2014-05-03 06:40:30 +0900

    script, production時のAPP_HOMEを2014に変更

--- script/carton.sh
+++ script/carton.sh
@@ -4,7 +4,7 @@ export MOJO_MODE=production
 export PLACK_ENV=production
 export LC_ALL=en_US
 export PATH=/opt/local/perl-5.16/bin:$PATH
-APP_HOME=/var/lib/jpa/yapcasia.org/yapcasia2013-app
+APP_HOME=/var/lib/jpa/yapcasia.org/yapcasia2014-app
 
 cd $APP_HOME
 exec carton exec -Ilib -- $@
