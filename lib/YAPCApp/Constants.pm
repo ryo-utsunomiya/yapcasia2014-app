770e8e96c01570204231c78e2653917d40042d4b
Yoshimitsu Torii <yoshimitsu.torii@gmail.com>
2014-07-04 19:10:11 +0900

    venue_idの対応を去年と入れ替え。今年はイベントホールを4にした

--- lib/YAPCApp/Constants.pm
+++ lib/YAPCApp/Constants.pm
@@ -9,15 +9,15 @@ use Exporter::Constants (
     \@EXPORT => +{
         VENUES => [
             { id => 1, name => 'Main Hall'},
-            { id => 2, name => 'Event Hall'},
-            { id => 3, name => 'Sub Room1'},
-            { id => 4, name => 'Sub Room2'},
+            { id => 2, name => 'Sub Room1'},
+            { id => 3, name => 'Sub Room2'},
+            { id => 4, name => 'Event Hall'},
         ],
         VENUE_ID2NAME => {
             1 => 'Main Hall',
-            2 => 'Event Hall',
-            3 => 'Sub Room1',
-            4 => 'Sub Room2',
+            2 => 'Sub Room1',
+            3 => 'Sub Room2',
+            4 => 'Event Hall',
         },
     },
 );
